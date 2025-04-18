11e91192acf5dd09fb0aab07e156b0e54167e0f4
kanderson <81449406+kandrsn99@users.noreply.github.com>
2025-04-18 17:44:27 -0500

    Update server.cpp

--- crow_app/server.cpp
+++ crow_app/server.cpp
@@ -23,9 +23,9 @@ crow::json::wvalue build_json_result(const pqxx::result& res) {
 int main() {
         crow::SimpleApp app;
         // Post a new employee
-        CROW_ROUTE(app, "/hire").methods("GET"_method)([](const crow::request &req) -> crow::response {
+        CROW_ROUTE(app, "/hire").methods("POST"_method)([](const crow::request &req) -> crow::response {
                 // Test method called by user.
-                if(req.method != crow::HTTPMethod::Get) {
+                if(req.method != crow::HTTPMethod::Post) {
                     // Wrong method sent.
                     return crow::response(405);
                 }
@@ -34,14 +34,35 @@ int main() {
                     // Open connection with pgbouncer
                     pqxx::connection conn("dbname=mydatabase user=myuser password=mypassword host=running_bouncer port=6432");
                     pqxx::work txn(conn);
-                    // Create our SQL query
-                    pqxx::result res = txn.exec("SELECT * FROM employees ORDER BY employee_id;");
+                    // Initialize the results.
+                    pqxx::result res;
+                    // Check for the parameters by initializing a pointer for the url sent.
+                    const char* first_parameter = req.url_params.get("first");
+                    const char* last_parameter = req.url_params.get("last");
+                    const char* vehicle_parameter = req.url_params.get("vehicle");
+                    // Check the parameters every time we are called up.
+                    if(first_parameter && last_parameter && vehicle_parameter) {
+                                // Parse our search value name parameter.
+                                std::string first_pattern = "%" + std::string(first_parameter) + "%";
+                                std::string last_pattern = "%" + std::string(last_parameter) + "%";
+                                std::string vehicle_pattern = "%" + std::string(vehicle_parameter) + "%";
+                                // Prepare SQL call.
+                                res = txn.exec_params("INSERT INTO employees"
+                                        " (first_name, last_name, vehicle)"
+                                        " VALUES first_name ILIKE $1, last_name ILIKE $2, vehicle ILIKE $3;",
+                                        first_pattern,
+                                        last_pattern,
+                                        vehicle_pattern
+                                        );
+                            INSERT INTO employees (first_name, last_name, vehicle) VALUES ($1, $2, $3);
+                            
+                    } else {
+                            return crow::reponse(400, std::string("Check input!"));
+                    }
                     // Transact
                     txn.commit();
-                    // Create JSON
-                    crow::json::wvalue build_json_result = build_system_json(res);
                     // Dump JSON out for endpoint.
-                    return crow::response(json_res.dump());
+                    return crow::response(201, std::string("Created!"));
                 } catch (const std::exception& e) {
                     // May check for more issues during development by adding + e.what()
                     return crow::response(500, std::string("Error!"));
@@ -50,7 +71,7 @@ int main() {
         // Put an update on an employee
         CROW_ROUTE(app, "/update").methods("PUT"_method)([](const crow::request &req) -> crow::response {
                 // Test method called by user.
-                if(req.method != crow::HTTPMethod::Get) {
+                if(req.method != crow::HTTPMethod::Put) {
                     // Wrong method sent.
                     return crow::response(405);
                 }
@@ -59,14 +80,33 @@ int main() {
                     // Open connection with pgbouncer
                     pqxx::connection conn("dbname=mydatabase user=myuser password=mypassword host=running_bouncer port=6432");
                     pqxx::work txn(conn);
-                    // Create our SQL query
-                    pqxx::result res = txn.exec("UPDATE employees SET vehicle = $3 WHERE first_name = $1 AND last_name = $2;");
+                    // Initialize the results.
+                    pqxx::result res;
+                    // Check for the parameters by initializing a pointer for the url sent.
+                    const char* first_parameter = req.url_params.get("first");
+                    const char* last_parameter = req.url_params.get("last");
+                    const char* vehicle_parameter = req.url_params.get("vehicle");
+                    // Check the parameters every time we are called up.
+                    if(first_parameter && last_parameter && vehicle_parameter) {
+                                // Parse our search value name parameter.
+                                std::string first_pattern = "%" + std::string(first_parameter) + "%";
+                                std::string last_pattern = "%" + std::string(last_parameter) + "%";
+                                std::string vehicle_pattern = "%" + std::string(vehicle_parameter) + "%";
+                                // Prepare SQL call.
+                                res = txn.exec_params("UPDATE employees"
+                                        " SET vehicle ILIKE $1"
+                                        " WHERE first_name ILIKE $2 AND last_name ILIKE $3;",
+                                        vehicle_pattern,
+                                        first_pattern,
+                                        last_pattern
+                                        );
+                    } else {
+                            return crow::reponse(400, std::string("Check input!"));
+                    }
                     // Transact
                     txn.commit();
-                    // Create JSON
-                    crow::json::wvalue build_json_result = build_system_json(res);
                     // Dump JSON out for endpoint.
-                    return crow::response(json_res.dump());
+                    return crow::response(200, std::string("Updated!"));
                 } catch (const std::exception& e) {
                     // May check for more issues during development by adding + e.what()
                     return crow::response(500, std::string("Error!"));
@@ -84,6 +124,8 @@ int main() {
                     // Open connection with pgbouncer
                     pqxx::connection conn("dbname=mydatabase user=myuser password=mypassword host=running_bouncer port=6432");
                     pqxx::work txn(conn);
+                    // Initialize the results.
+                    pqxx::result res;
                     // Check for the parameters by initializing a pointer for the url sent.
                     const char* first_parameter = req.url_params.get("first");
                     const char* last_parameter = req.url_params.get("last");
